420b798b79876bb50bd4e0ad3c014fbc0e2662ce
Ronald Macario <ronaldmacario13@gmail.com>
2023-08-20 18:10:46 -0300

    feat: adicionando funcao de carregar todos os produtos

--- frontend/src/screen/home/Home.tsx
+++ frontend/src/screen/home/Home.tsx
@@ -1,8 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-import { Text } from 'react-native';
+import { Text, View } from 'react-native';
+import listarProdutos from "../../services/listarProdutos/listarProdutos";
+
+// Pagina home, primeira página mostrada ao cliente logado, mostra todos os produtos.
 
 export function Home() {
+    
+    // Parte reponsavel carregar os produtos, usando a funcao listarProdutos.
+
+    const [produtos, setProdutos] = useState([]);
+    
+    useEffect(() => {
+        async function carregarProdutos() {
+            try {
+                const produtosData = await listarProdutos();
+                setProdutos(produtosData);
+            } catch (error) {
+                console.log("Passou2")
+                console.error('Erro ao carregar produtos:', error);
+        }
+      }
+  
+      carregarProdutos();
+    }, []);
+
+    
   return (
     <>
       <Text>
