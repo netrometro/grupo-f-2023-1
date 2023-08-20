d09fe0133baa2d6596786b69f2e64d7fcf51f071
Ronald Macario <ronaldmacario13@gmail.com>
2023-08-20 16:32:40 -0300

    feat: apagando exemplo e adicionando a tela teste de Home

--- frontend/App.js
+++ frontend/App.js
@@ -1,20 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Home } from './src/screen/home/Home.tsx'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <Home />
     </View>
+
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
