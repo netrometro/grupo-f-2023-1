d333c549968578fefd01f3dc21815988a6ccd34b
David Rocha <manoeudavi3@gmail.com>
2023-08-20 17:39:41 -0300

    criado componente de doacoes

---
+++ frontend/src/screen/home/Itens.tsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { View, Text, TextInput, Picker, Button, StyleSheet } from 'react-native';
+
+export function Itens() {
+  return (
+    <>
+      <Text>
+        TESTE
+      </Text>
+    </>
+  );
+}
