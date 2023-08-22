ccaa7ee2456d22bd801494f0f415325a58b95d2f
David Rocha <manoeudavi3@gmail.com>
2023-08-22 15:38:00 -0300

    criado estrtuta inicial de view

--- frontend/src/screen/donateItens/DonateArea.tsx
+++ frontend/src/screen/donateItens/DonateArea.tsx
@@ -1,14 +1,29 @@
 import React from "react";
 import { View, Text } from "react-native";
+import Estilo from "./Styles";
 
 export function DonateArea() {
-    
-    
   return (
-   <View>
-       <Text>
-            Teste da minha tela de doação
-       </Text>
-   </View>
+    <View style={Estilo.container}>
+      <View style={Estilo.imagem}>
+        <Text>Imagem</Text>
+        <View>
+          <Text>Adicione imagem aqui</Text>
+        </View>
+      </View>
+      <View style={Estilo.produtoTitulo}>
+        <Text>titulo</Text>
+        
+      </View>
+      <View style={Estilo.produtoCategoria}>
+        <Text>categoria</Text>
+      </View>
+      <View style={Estilo.produtoDescricao}>
+        <Text>descrição</Text>
+      </View>
+      <View style={Estilo.botaoDoar}>
+        <Text>doar</Text>
+      </View>
+    </View>
   );
 }
---
+++ frontend/src/screen/donateItens/Styles.tsx
@@ -0,0 +1,43 @@
+import { StyleSheet} from "react-native";
+
+export default StyleSheet.create({
+    container:{
+        backgroundColor:"#191924",
+        flex:1,
+        display:"flex",
+        alignItems:"center",
+        gap:20
+        
+        
+    },
+
+    imagem:{
+        backgroundColor:"blue",
+        width:150,
+        height:150,
+        display:"flex",
+        alignItems:"center",
+        justifyContent:"space-evenly"
+    },
+    produtoTitulo:{
+        backgroundColor:"orange",
+        width:300,
+        height:100,
+    },
+    produtoDescricao:{
+        backgroundColor:"green",
+        width:150,
+        height:150,
+    },
+    produtoCategoria:{
+        backgroundColor:"white",
+        width:150,
+        height:150,
+    },
+    botaoDoar:{
+        backgroundColor:"grey",
+        width:150,
+        height:150,
+    }
+})
+
