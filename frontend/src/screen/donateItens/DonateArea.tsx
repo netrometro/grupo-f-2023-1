ebf9f43c18cb4736100af62e5bf3eca4c1b68686
David Rocha <manoeudavi3@gmail.com>
2023-08-22 18:23:41 -0300

    adicionado as cores aos campos

--- frontend/src/screen/donateItens/DonateArea.tsx
+++ frontend/src/screen/donateItens/DonateArea.tsx
@@ -1,28 +1,78 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React, { useState } from "react";
+import { View, Text, TextInput, KeyboardAvoidingView } from "react-native";
+import { RadioButton } from "react-native-paper";
 import Estilo from "./Styles";
 
 export function DonateArea() {
+  const [selectedCategory, setSelectedCategory] = useState(""); // Estado para rastrear a categoria selecionada
+
   return (
     <View style={Estilo.container}>
       <View style={Estilo.imagem}>
-        <Text>Imagem</Text>
+        <Text style={Estilo.imagemText}>Imagem</Text>
         <View>
-          <Text>Adicione imagem aqui</Text>
+          <Text style={Estilo.imagemText}>Adicione imagem aqui</Text>
         </View>
       </View>
-      <View style={Estilo.produtoTitulo}>
-        <Text>titulo</Text>
-        
+
+      <View style={Estilo.tituloContainer}>
+        <Text style={Estilo.tituloText}>Título:</Text>
+        <TextInput style={Estilo.tituloImput} />
       </View>
-      <View style={Estilo.produtoCategoria}>
-        <Text>categoria</Text>
+
+      <View style={Estilo.categoriaContainer}>
+        <Text style={Estilo.categoriaText}>Categoria:</Text>
+        <View style={Estilo.categoriaRadio}>
+          <View style={Estilo.TextAndRadio}>
+            <RadioButton
+              value="Roupas"
+              status={selectedCategory === "Roupas" ? "checked" : "unchecked"}
+              color={selectedCategory === "checked" ? "#808080" : "#7353ED"}
+              onPress={() => setSelectedCategory("Roupas")}
+            />
+            <Text style={Estilo.textRaio}>Roupas</Text>
+          </View>
+          <View style={Estilo.TextAndRadio}>
+            <RadioButton
+              color={selectedCategory === "checked" ? "#808080" : "#7353ED"}
+              value="Alimentos"
+              status={
+                selectedCategory === "Alimentos" ? "checked" : "unchecked"
+              }
+              onPress={() => setSelectedCategory("Alimentos")}
+            />
+            <Text style={Estilo.textRaio}>Alimentos</Text>
+          </View>
+          <View style={Estilo.TextAndRadio}>
+            <RadioButton
+              color={selectedCategory === "checked" ? "#808080" : "#7353ED"}
+              value="Brnquedos"
+              status={
+                selectedCategory === "Brinquedos" ? "checked" : "unchecked"
+              }
+              onPress={() => setSelectedCategory("Brinquedos")}
+            />
+            <Text style={Estilo.textRaio}>Brinquedos</Text>
+          </View>
+          <View style={Estilo.TextAndRadio}>
+            <RadioButton
+              color={selectedCategory === "checked" ? "#808080" : "#7353ED"}
+              value="Livros"
+              status={selectedCategory === "Livros" ? "checked" : "unchecked"}
+              onPress={() => setSelectedCategory("Livros")}
+            />
+            <Text style={Estilo.textRaio}>Livros</Text>
+          </View>
+        </View>
       </View>
-      <View style={Estilo.produtoDescricao}>
-        <Text>descrição</Text>
+
+      <View style={Estilo.descricaoContainer}>
+        <Text style={Estilo.descricaoText}>Descrição</Text>
+        <TextInput style={Estilo.descricaoImput} />
       </View>
-      <View style={Estilo.botaoDoar}>
-        <Text>doar</Text>
+
+      <View style={Estilo.botaoDoarContainer}>
+        <Text>Doar</Text>
       </View>
     </View>
   );
--- frontend/src/screen/donateItens/Styles.tsx
+++ frontend/src/screen/donateItens/Styles.tsx
@@ -1,43 +1,89 @@
-import { StyleSheet} from "react-native";
+import { StyleSheet } from "react-native";
 
 export default StyleSheet.create({
-    container:{
-        backgroundColor:"#191924",
-        flex:1,
-        display:"flex",
-        alignItems:"center",
-        gap:20
-        
-        
-    },
+  container: {
+    backgroundColor: "#191924",
+    flex: 1,
+    display: "flex",
+    alignItems: "center",
+    gap: 20,
+  },
 
-    imagem:{
-        backgroundColor:"blue",
-        width:150,
-        height:150,
-        display:"flex",
-        alignItems:"center",
-        justifyContent:"space-evenly"
-    },
-    produtoTitulo:{
-        backgroundColor:"orange",
-        width:300,
-        height:100,
-    },
-    produtoDescricao:{
-        backgroundColor:"green",
-        width:150,
-        height:150,
-    },
-    produtoCategoria:{
-        backgroundColor:"white",
-        width:150,
-        height:150,
-    },
-    botaoDoar:{
-        backgroundColor:"grey",
-        width:150,
-        height:150,
-    }
-})
+  imagem: {
+    backgroundColor: "blue",
+    width: 150,
+    height: 150,
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "space-evenly",
+  },
+  imagemText: {},
 
+  tituloContainer: {
+
+    width: 330,
+    height: 100,
+    display: "flex",
+    gap:10,
+  },
+
+  tituloText: {
+    color:"white"
+  },
+
+  tituloImput: {
+    backgroundColor:"#808080",
+    borderRadius:12,
+    height:50,
+    paddingLeft:15,
+    color:"#191924",
+    fontWeight:"bold",
+    
+  },
+
+  categoriaContainer: {
+
+    width: 330,
+    height: 100,
+    display: "flex",
+    gap:10,
+  },
+  categoriaText: {
+    color:"white"
+  },
+  categoriaRadio: {
+
+   display:"flex",
+   flexDirection:"row",
+   justifyContent:"space-evenly"
+  },
+
+  textRaio:{
+    color:"white"
+  },
+
+  TextAndRadio:{
+    display:"flex",
+    alignItems:"center"
+  },
+
+  descricaoContainer: {
+    width: 330,
+    height: 150,
+    gap:10,
+  },
+  descricaoText: {
+    color:"white"
+  },
+  descricaoImput: {
+    backgroundColor:"#808080",
+    height:120,
+    borderRadius:12
+  },
+
+  botaoDoarContainer: {
+    backgroundColor: "grey",
+    width: 150,
+    height: 150,
+  },
+});
