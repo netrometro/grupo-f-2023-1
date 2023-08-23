c0a34bc9e8c29bd21d654e80cf5a42afeee059f8
Ronald Macario <ronaldmacario13@gmail.com>
2023-08-22 22:19:45 -0300

    feat: adicionando header para a página Home

---
+++ frontend/src/components/home/header/index.tsx
@@ -0,0 +1,15 @@
+import { Text, View } from 'react-native';
+import estilo from "./styles";
+import React from 'react';
+
+
+export function Header() {
+
+    return (
+        <View style={estilo.header}>
+            <Text style={estilo.textoPrincipal}>Olá, Rônald.</Text>
+            <Text style={estilo.textoSecundario}>Quer ajudar ou precisa de ajuda? Aqui é o lugar certo.</Text>
+        </View>
+    );
+}
+
