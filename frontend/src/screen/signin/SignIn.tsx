7ae2cf0417b3d4236e573b903e09ce5089feb3a9
Ronald Macario <ronaldmacario13@gmail.com>
2023-08-23 16:14:24 -0300

    feat: fazendo front para o cadastro de users

--- frontend/src/screen/signin/SignIn.tsx
+++ frontend/src/screen/signin/SignIn.tsx
@@ -5,13 +5,32 @@ import { Button, View, Text } from 'react-native';
 
 export function SignIn({navigation}) {
     return (
-        <View>
-            <View>
-                <Text>
-                    TELA INICIAL DO LOGIN
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium voluptatum eius voluptatibus quo, quas quaerat sed veniam minus molestiae et odio placeat libero aperiam quos. Eaque distinctio sequi officiis ipsa.
-                </Text>
-            </View>
+        <View style={estilo.container}>
+            <Text style={estilo.titulo  }>CADASTRAR</Text>
+            <TextInput
+                value={Nome}
+                onChangeText={(Nome) => setNome(Nome)}
+                placeholder={'Nome'}
+                style={estilo.input}
+            />
+            <TextInput
+                value={Contato}
+                onChangeText={(Contato) => setContato(Contato)}
+                placeholder={'Contato'}
+                style={estilo.input}
+            />
+            <TextInput
+                value={Endereco}
+                onChangeText={(Endereco) => setEndereco (Endereco)}
+                placeholder={'Endereco'}
+                style={estilo.input}
+            />
+            <TouchableOpacity
+            style={estilo.botao}
+            onPress={handleCadastrar}
+            >
+                <Text>Cadastrar</Text>
+            </TouchableOpacity>
         </View>
     );
 }
