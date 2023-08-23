85bad008c3854960fd3d28e6b4cf1098132c54d7
Ronald Macario <ronaldmacario13@gmail.com>
2023-08-23 16:15:05 -0300

    feat: fazendo o front ficar funcional

--- frontend/src/screen/signin/SignIn.tsx
+++ frontend/src/screen/signin/SignIn.tsx
@@ -1,9 +1,20 @@
 import { useNavigation} from '@react-navigation/native';
-import React from 'react';
-import { Button, View, Text } from 'react-native';
+import React, { useState } from 'react';
+import { Button, View, Text, TextInput, TouchableOpacity } from 'react-native';
+import estilo from "./styles";
 
 
 export function SignIn({navigation}) {
+    const [Nome, setNome] = useState('');
+    const [Contato, setContato] = useState('');
+    const [Endereco, setEndereco] = useState('');
+
+    const handleCadastrar = () => {
+        console.log('Nome:', Nome);
+        console.log('Contato:', Contato);
+        console.log('Endereco:', Endereco);
+      };
+
     return (
         <View style={estilo.container}>
             <Text style={estilo.titulo  }>CADASTRAR</Text>
