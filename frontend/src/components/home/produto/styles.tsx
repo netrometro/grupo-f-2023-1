f84f8853510b0b40112096e011393a010443121c
Ronald Macario <ronaldmacario13@gmail.com>
2023-08-22 22:22:35 -0300

    feat: estilizando produto da home

---
+++ frontend/src/components/home/produto/styles.tsx
@@ -0,0 +1,13 @@
+import { StyleSheet} from "react-native";
+
+export default StyleSheet.create({
+    produto:{
+        backgroundColor: '#7353ED',
+        height: 140,
+        width: 140,
+        margin: 21,
+        borderRadius: 18,
+        alignSelf: 'center',
+        flexDirection: "column-reverse",
+        alignItems: "center"
+    },
