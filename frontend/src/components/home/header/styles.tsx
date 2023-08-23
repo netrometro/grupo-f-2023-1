6d364bc9886c278e0b82632278318824d3f31018
Ronald Macario <ronaldmacario13@gmail.com>
2023-08-22 22:20:32 -0300

    feat: estilização do header

---
+++ frontend/src/components/home/header/styles.tsx
@@ -0,0 +1,12 @@
+import { StyleSheet} from "react-native";
+
+export default StyleSheet.create({
+    header:{
+        backgroundColor: '#232231',
+        paddingTop: 10,
+        height: 130,
+        width: 350,
+        margin: 21,
+        borderRadius: 18,
+        alignSelf: 'center',
+    },
