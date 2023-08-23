a63c42d13893f73a68bd58234a266643418395b7
Ronald Macario <ronaldmacario13@gmail.com>
2023-08-22 22:23:25 -0300

    feat: estilizando página Home

---
+++ frontend/src/screen/home/Styles.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet} from "react-native";
+
+export default StyleSheet.create({
+    container:{
+        flex: 1,
+        backgroundColor: "#191924"
+    },
+    titulo:{
+        paddingBottom: 20,
+        paddingLeft: 35,
+        color: 'white',
+        fontSize: 18,
+    }
+})
