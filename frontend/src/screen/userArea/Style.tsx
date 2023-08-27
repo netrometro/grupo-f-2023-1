9670ff2ddfdf1191182d9859b091c1895e730470
David Rocha <manoeudavi3@gmail.com>
2023-08-27 17:30:53 -0300

    Criado estrutura para a tela de usuário

--- frontend/src/screen/userArea/Style.tsx
+++ frontend/src/screen/userArea/Style.tsx
@@ -4,10 +4,45 @@ import { Button } from "react-native-paper";
 export default StyleSheet.create({
     container:{
         flex: 1,
-        backgroundColor: "#191924"
+        backgroundColor: "#191924",
+        alignItems:"center"
+     
+        
+    },
+    container_son:{
+        flex: .4,
+        backgroundColor:"#7353ED",
+        alignItems:"center",
+        borderBottomLeftRadius:60,
+        borderBottomRightRadius:60,
+        width:"100%",
+        justifyContent:"space-evenly",
+        marginBottom:30
+    },
+    userImage:{
+        width:120,
+        height:120,
+        borderRadius:100,
+        borderWidth:2,
+        borderColor:"#232231"
+    },
+
+    user_text_info:{
+        fontWeight:"bold",
+        letterSpacing:1
+    },
+   
+    container_text_and_button:{
+        backgroundColor:"orange",
+        width:"100%",
+        height:40,
+
+    },
+    buttonEdit:{
+        width:200,
+        marginTop:50
     },
     buttonLogout:{
         width:200,
-        
     }
 })
--- frontend/src/screen/userArea/User.tsx
+++ frontend/src/screen/userArea/User.tsx
@@ -1,10 +1,9 @@
 import React from "react";
 import estilo from "./Style";
-import { View, Text } from "react-native";
+import { View, Text, Image } from "react-native";
 import { Button } from "react-native-paper";
 import { getAuth, signOut } from "firebase/auth";
-import { useNavigation } from '@react-navigation/native';
-
+import { useNavigation } from "@react-navigation/native";
 
 export function User() {
   const navigation = useNavigation();
@@ -13,18 +12,29 @@ export function User() {
     signOut(auth)
       .then(() => {
         // @ts-ignore
-        navigation.navigate("TelaInicial")
+        navigation.navigate("TelaInicial");
       })
       .catch((error) => {
-        console.log(error)
+        console.log(error);
       });
   }
 
   return (
     <View style={estilo.container}>
-      <Button mode="contained" onPress={() => {logout()}}>
+      <View style={estilo.container_son}>
+        <Image style={estilo.userImage} source={require("../../assets/david.jpg")}></Image>
+        <Text style = {estilo.user_text_info}>David Emmanoel Lopes Rocha</Text>
+        <Text>manoeudavi3@gmail.com</Text>
+        <Text>(87) 9 96269050</Text>
+        <Text>(87) Garanhuns, PE, Brasil</Text>
+      </View>
+       <Button style={estilo.buttonLogout} mode="contained" onPress={() => {}}>
+        Editar Perfil
+      </Button>
+      <Button buttonColor="#D65353" style={estilo.buttonLogout} mode="contained" onPress={() => {logout()}}>
         Sair da Conta
       </Button>
+     
     </View>
   );
 }
