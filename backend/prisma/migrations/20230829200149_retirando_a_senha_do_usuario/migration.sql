7b6ddc394a2d88958e407b4ffb2d64cd62647cd3
Ronald Macario <ronaldmacario13@gmail.com>
2023-08-29 17:06:42 -0300

    fix: removendo a senha do bd

---
+++ backend/prisma/migrations/20230829200149_retirando_a_senha_do_usuario/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `senha` on the `Usuario` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Usuario" DROP COLUMN "senha";
