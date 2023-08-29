e2dbadefb80385b62efcab341bde46f1531c2910
Ronald Macario <ronaldmacario13@gmail.com>
2023-08-29 20:59:40 -0300

    feat: desenvolvendo o front do cadastro de perfil

--- frontend/src/screen/perfilArea/Perfil.tsx
+++ frontend/src/screen/perfilArea/Perfil.tsx
@@ -4,8 +4,29 @@ import { View, Text } from 'react-native'
 
 export  function Perfil() {
   return (
-    <View>
-      <Text>Testando</Text>
+    <View style={estilo.container}>
+      <Text style={estilo.titulo}>Cadastro</Text>
+
+      <TextInput
+        placeholder="Nome"
+        value={nome}
+        onChangeText={setNome}
+        style={estilo.input}
+      />
+      <TextInput
+        placeholder="Endereço"
+        value={endereco}
+        onChangeText={setEndereco}
+        style={estilo.input}
+      />
+      <TextInput
+        placeholder="contato"
+        value={contato}
+        onChangeText={setContato}
+        style={estilo.input}
+      />
+      <Button title="Cadastrar" onPress={handleCadastro} />
+
     </View>
   )
 }
