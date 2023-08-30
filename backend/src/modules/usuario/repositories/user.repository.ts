c56212747e736f98a6102245773e8857daee503c
Ronald Macario <ronaldmacario13@gmail.com>
2023-08-29 21:04:27 -0300

    feat: criando função para buscar por user por id

--- backend/src/modules/usuario/repositories/user.repository.ts
+++ backend/src/modules/usuario/repositories/user.repository.ts
@@ -1,7 +1,6 @@
 import prisma from "../../../utils/prisma";
 import { createUserInput} from "../schema/user.schema";
 
-
 //Criar usuario
 export async function createUser(data: createUserInput){
     const user = await prisma.usuario.create({
@@ -13,4 +12,13 @@ export async function createUser(data: createUserInput){
 //listar usuário
 export async function findUsers(){
     return prisma.usuario.findMany()
+}
+
+//listar por ID
+export async function findUsersById(id: string) {
+    return prisma.usuario.findUnique({
+        where:{
+            id
+        }
+    })
 }
